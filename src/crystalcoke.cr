28ad4742e5b11db594334c0282633dcc31e5b9a1
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-24 21:09:01 -0400

    basic stuff

--- src/crystalcoke.cr
+++ src/crystalcoke.cr
@@ -1,3 +1,23 @@
+require "phreak"
+require "yaml"
+shard = YAML.parse(File.read("./shard.yml"))
+
 module Crystalcoke
-  puts "Hello world!"
+  Phreak.parse! do |cc|
+    cc.banner = "A really stupid and kinda dank CLI as a \"practical\" project for Crystal."
+
+    cc.bind(short_flag: 'n', long_flag: "nothing",
+      description: "Do absolutely nothing.") do
+    end
+
+    cc.bind(short_flag: 'h', long_flag: "help",
+      description: "Shows all the available commands and flags.") do
+      puts cc
+    end
+
+    # Print commands when there is no commands or arguments
+    cc.default do
+      puts cc
+    end
+  end
 end
