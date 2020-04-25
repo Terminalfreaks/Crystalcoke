d62c7c9a9e890a7e84158e905250a5b3251b7b6d
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-25 10:22:51 -0400

    my own weird way of doing this and it is for a specific reason yes

--- src/crystalcoke.cr
+++ src/crystalcoke.cr
@@ -1,3 +1,4 @@
+require "option_parser"
 require "phreak"
 require "yaml"
 require "json"
@@ -6,47 +7,29 @@ list = JSON.parse(File.read("./src/utils/copypasta.json"))
 
 module Crystalcoke
   Phreak.parse! do |cc|
-    cc.banner = "A really stupid and kinda dank CLI as a \"practical\" project for Crystal."
+    cc.banner = "A really stupid and dank CLI as a \"practical\" project for Crystal."
 
-    cc.bind(short_flag: 'n', long_flag: "nothing",
-      description: "Do absolutely nothing.") do
-    end
-
-    cc.bind(short_flag: 'h', long_flag: "help",
-      description: "Shows all the available commands and flags.") do
+    # Help command
+    cc.bind(word: "help", short_flag: 'h') do
       puts cc
     end
 
-    cc.bind(word: "ping", description: "Pings idk nasa its a stupid command") do
-      puts "pong! 69ms bro"
+    # Does literally nothing
+    cc.bind(word: "nothing", short_flag: 'n') do
     end
 
-    cc.bind(word: "copypasta", description: "Get your favourite copypasta!!!") do |pasta|
-      pasta.bind(word: "help", description: "List all copypasta sub commands") do
-        puts pasta
-      end
+    cc.bind(word: "ping", short_flag: 'p') do
+      puts "pong! 69ms"
+    end
 
-      pasta.bind(word: "list", description: "Lists all the availble copypasta") do
-        pastamenu = list["copypasta"].as_h
-        pastalist = [] of String
+    cc.bind(word: "copypasta", short_flag: 'c') do
+      OptionParser.parse do |e|
+        e.banner = "f"
 
-        pastamenu.each do |key, value|
-          pastalist << key
+        e.on "--pasta", "i want to die" do
+          puts "testing"
         end
-
-        puts "Here is a list of the copypasta u can use.\nRun it with \"crystalcoke copypasta <pasta>.\""
-        puts ""
-        puts pastalist.join("\n")
       end
-
-      pasta.missing_args do
-        puts pasta
-      end
-    end
-
-    # Print commands when there is no commands or arguments
-    cc.default do
-      puts cc
     end
   end
 end
